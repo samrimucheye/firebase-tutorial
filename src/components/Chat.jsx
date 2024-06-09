6bc14c5cd59fa2adfa8b51b7d4d13bd65b7b2987
Samri Mucheye <samrimucheye@gmail.com>
2024-06-09 10:43:09 +0300

    added new world in button

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -69,7 +69,7 @@ const Chat = () => {
             onChange={(e) => setMessage(e.target.value)}
           />
           <button type="submit" className="btn bg-blue-400">
-            send Message please
+            send Message please now
           </button>
         </form>
       </div>
