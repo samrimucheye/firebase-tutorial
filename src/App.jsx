3f9ee550a67b549cb871b9a009e4261f11d69f17
Samri Mucheye <samrimucheye@gmail.com>
2024-06-11 15:59:30 +0300

    added change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="bg-yellow-600 min-h-screen p-4 my-9">
-        <h2>welcome to malede gashaw's chat</h2>
+        <h2>welcome to malede gashaw's chats</h2>
         <Routes>
           <Route
             path="/"
